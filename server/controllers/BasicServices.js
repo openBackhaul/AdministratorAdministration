d6f938f67722646ebb89d3c743841fd6bdef5772
ManasaBM <manasa.b-m.external@telefonica.com>
2023-07-05 07:43:50 +0200

    Resolve /v1/register-yourself service RequestHeaders require changes when RequestBody is empty Fixes #257

--- server/controllers/BasicServices.js
+++ server/controllers/BasicServices.js
@@ -24,7 +24,7 @@ module.exports.embedYourself = async function embedYourself(req, res, next, body
         responseBodyToDocument = sentResp.body;
       });
     executionAndTraceService.recordServiceRequest(xCorrelator, traceIndicator, user, originator, req.url, responseCode, req.body, responseBodyToDocument);
-  } catch (error) {}
+  } catch (error) { }
 
 };
 
@@ -46,11 +46,11 @@ module.exports.endSubscription = async function endSubscription(req, res, next, 
         responseBodyToDocument = sentResp.body;
       });
     executionAndTraceService.recordServiceRequest(xCorrelator, traceIndicator, user, originator, req.url, responseCode, req.body, responseBodyToDocument);
-  } catch (error) {}
+  } catch (error) { }
 
 };
 
-module.exports.startApplicationInGenericRepresentation = async function startApplicationInGenericRepresentation (req, res, next, user, originator, xCorrelator, traceIndicator, customerJourney) {
+module.exports.startApplicationInGenericRepresentation = async function startApplicationInGenericRepresentation(req, res, next, user, originator, xCorrelator, traceIndicator, customerJourney) {
   try {
     let startTime = process.hrtime();
     let responseCode = responseCodeEnum.code.OK;
@@ -68,7 +68,7 @@ module.exports.startApplicationInGenericRepresentation = async function startApp
         responseBodyToDocument = sentResp.body;
       });
     executionAndTraceService.recordServiceRequest(xCorrelator, traceIndicator, user, originator, req.url, responseCode, req.body, responseBodyToDocument);
-  } catch (error) {}
+  } catch (error) { }
 };
 
 
@@ -90,7 +90,7 @@ module.exports.informAboutApplication = async function informAboutApplication(re
         responseBodyToDocument = sentResp.body;
       });
     executionAndTraceService.recordServiceRequest(xCorrelator, traceIndicator, user, originator, req.url, responseCode, req.body, responseBodyToDocument);
-  } catch (error) {}
+  } catch (error) { }
 
 };
 
@@ -112,7 +112,7 @@ module.exports.informAboutApplicationInGenericRepresentation = async function in
         responseBodyToDocument = sentResp.body;
       });
     executionAndTraceService.recordServiceRequest(xCorrelator, traceIndicator, user, originator, req.url, responseCode, req.body, responseBodyToDocument);
-  } catch (error) {}
+  } catch (error) { }
 
 };
 
@@ -134,7 +134,7 @@ module.exports.informAboutReleaseHistory = async function informAboutReleaseHist
         responseBodyToDocument = sentResp.body;
       });
     executionAndTraceService.recordServiceRequest(xCorrelator, traceIndicator, user, originator, req.url, responseCode, req.body, responseBodyToDocument);
-  } catch (error) {}
+  } catch (error) { }
 
 };
 
@@ -156,7 +156,7 @@ module.exports.informAboutReleaseHistoryInGenericRepresentation = async function
         responseBodyToDocument = sentResp.body;
       });
     executionAndTraceService.recordServiceRequest(xCorrelator, traceIndicator, user, originator, req.url, responseCode, req.body, responseBodyToDocument);
-  } catch (error) {}
+  } catch (error) { }
 
 };
 
@@ -178,7 +178,7 @@ module.exports.inquireOamRequestApprovals = async function inquireOamRequestAppr
         responseBodyToDocument = sentResp.body;
       });
     executionAndTraceService.recordServiceRequest(xCorrelator, traceIndicator, user, originator, req.url, responseCode, req.body, responseBodyToDocument);
-  } catch (error) {}
+  } catch (error) { }
 
 };
 
@@ -200,7 +200,7 @@ module.exports.listLtpsAndFcs = async function listLtpsAndFcs(req, res, next, us
         responseBodyToDocument = sentResp.body;
       });
     executionAndTraceService.recordServiceRequest(xCorrelator, traceIndicator, user, originator, req.url, responseCode, req.body, responseBodyToDocument);
-  } catch (error) {}
+  } catch (error) { }
 
 };
 
@@ -209,7 +209,7 @@ module.exports.redirectOamRequestInformation = async function redirectOamRequest
     let startTime = process.hrtime();
     let responseCode = responseCodeEnum.code.NO_CONTENT;
     let responseBodyToDocument = {};
-    await BasicServices.redirectOamRequestInformation(body,user, originator, xCorrelator, traceIndicator, customerJourney, req.url)
+    await BasicServices.redirectOamRequestInformation(body, user, originator, xCorrelator, traceIndicator, customerJourney, req.url)
       .then(async function (responseBody) {
         responseBodyToDocument = responseBody;
         let responseHeader = await restResponseHeader.createResponseHeader(xCorrelator, startTime, req.url);
@@ -222,7 +222,7 @@ module.exports.redirectOamRequestInformation = async function redirectOamRequest
         responseBodyToDocument = sentResp.body;
       });
     executionAndTraceService.recordServiceRequest(xCorrelator, traceIndicator, user, originator, req.url, responseCode, req.body, responseBodyToDocument);
-  } catch (error) {}
+  } catch (error) { }
 
 };
 
@@ -244,7 +244,7 @@ module.exports.redirectServiceRequestInformation = async function redirectServic
         responseBodyToDocument = sentResp.body;
       });
     executionAndTraceService.recordServiceRequest(xCorrelator, traceIndicator, user, originator, req.url, responseCode, req.body, responseBodyToDocument);
-  } catch (error) {}
+  } catch (error) { }
 
 };
 
@@ -266,7 +266,7 @@ module.exports.redirectTopologyChangeInformation = async function redirectTopolo
         responseBodyToDocument = sentResp.body;
       });
     executionAndTraceService.recordServiceRequest(xCorrelator, traceIndicator, user, originator, req.url, responseCode, req.body, responseBodyToDocument);
-  } catch (error) {}
+  } catch (error) { }
 
 };
 
@@ -275,11 +275,13 @@ module.exports.registerYourself = async function registerYourself(req, res, next
     let startTime = process.hrtime();
     let responseCode = responseCodeEnum.code.NO_CONTENT;
     let responseBodyToDocument = {};
-    if (body["registry-office-application"] === undefined) {
-      customerJourney = traceIndicator;
-      traceIndicator = xCorrelator;
-      xCorrelator = originator;
-      user = body;
+    if (Object.keys(req.body).length === 0) {
+      body = req.body;
+      user = req.headers["user"];
+      originator = req.headers["originator"];
+      xCorrelator = req.headers["x-correlator"];
+      traceIndicator = req.headers["trace-indicator"];
+      customerJourney = req.headers["customer-journey"];
     }
     await BasicServices.registerYourself(body, user, originator, xCorrelator, traceIndicator, customerJourney, req.url)
       .then(async function (responseBody) {
@@ -294,7 +296,7 @@ module.exports.registerYourself = async function registerYourself(req, res, next
         responseBodyToDocument = sentResp.body;
       });
     executionAndTraceService.recordServiceRequest(xCorrelator, traceIndicator, user, originator, req.url, responseCode, req.body, responseBodyToDocument);
-  } catch (error) {}
+  } catch (error) { }
 
 };
 
@@ -316,7 +318,7 @@ module.exports.updateClient = async function updateClient(req, res, next, body, 
         responseBodyToDocument = sentResp.body;
       });
     executionAndTraceService.recordServiceRequest(xCorrelator, traceIndicator, user, originator, req.url, responseCode, req.body, responseBodyToDocument);
-  } catch (error) {}
+  } catch (error) { }
 
 };
 
@@ -338,7 +340,7 @@ module.exports.updateOperationClient = async function updateOperationClient(req,
         responseBodyToDocument = sentResp.body;
       });
     executionAndTraceService.recordServiceRequest(xCorrelator, traceIndicator, user, originator, req.url, responseCode, req.body, responseBodyToDocument);
-  } catch (error) {}
+  } catch (error) { }
 
 };
 
@@ -360,6 +362,6 @@ module.exports.updateOperationKey = async function updateOperationKey(req, res, 
         responseBodyToDocument = sentResp.body;
       });
     executionAndTraceService.recordServiceRequest(xCorrelator, traceIndicator, user, originator, req.url, responseCode, req.body, responseBodyToDocument);
-  } catch (error) {}
+  } catch (error) { }
 
 };
