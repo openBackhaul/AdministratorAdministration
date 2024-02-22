41b0ba0e7ff250667b71e9ff814c699a3ad35e5c
ManasaBM1 <manasa.b-m.external@telefonica.com>
2024-02-22 13:32:54 +0530

    modify the port to 3028

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ var path = require('path');
 var http = require('http');
 var oas3Tools = require('openbackhaul-oas3-tools');
 var appCommons = require('onf-core-model-ap/applicationPattern/commons/AppCommons');
-var serverPort = 3004;
+var serverPort = 3028;
 
 // uncomment if you do not want to validate security e.g. operation-key, basic auth, etc
 // appCommons.openApiValidatorOptions.validateSecurity = false;
